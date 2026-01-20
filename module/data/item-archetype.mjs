df25d860298d40a18296f46bc0e1275608252026
MrTheBino <mrthebino@gmail.com>
2026-01-20 12:04:44 +0100

    default values for archetypes

--- module/data/item-archetype.mjs
+++ module/data/item-archetype.mjs
@@ -13,46 +13,46 @@ export default class ArkhamHorrorArchetype extends ArkhamHorrorItemBase {
     schema.sourceBook = new fields.StringField({ required: true, nullable: false, blank: true, initial: "" });
 
     schema.skillCaps = new fields.SchemaField({
-      agility: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      athletics: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      wits: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      presence: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      intuition: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      knowledge: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      resolve: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      meleeCombat: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      rangedCombat: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-      lore: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 })
+      agility: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      athletics: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      wits: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      presence: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      intuition: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      knowledge: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      resolve: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      meleeCombat: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      rangedCombat: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 }),
+      lore: new fields.NumberField({ ...requiredInteger, initial: 4, min: 0 })
     });
 
     schema.knackTiers = new fields.SchemaField({
       1: new fields.SchemaField({
-        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-        xpcost: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 3, min: 0 }),
+        xpcost: new fields.NumberField({ ...requiredInteger, initial: 3, min: 0 }),
         allowedKnacks: new fields.ArrayField(new fields.SchemaField({
           uuid: new DocumentUUIDField({ required: true, nullable: false, blank: false }),
           tier: new fields.NumberField({ ...requiredTier, initial: 1 })
         }))
       }),
       2: new fields.SchemaField({
-        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-        xpcost: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 2, min: 0 }),
+        xpcost: new fields.NumberField({ ...requiredInteger, initial: 6, min: 0 }),
         allowedKnacks: new fields.ArrayField(new fields.SchemaField({
           uuid: new DocumentUUIDField({ required: true, nullable: false, blank: false }),
           tier: new fields.NumberField({ ...requiredTier, initial: 2 })
         }))
       }),
       3: new fields.SchemaField({
-        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-        xpcost: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 2, min: 0 }),
+        xpcost: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
         allowedKnacks: new fields.ArrayField(new fields.SchemaField({
           uuid: new DocumentUUIDField({ required: true, nullable: false, blank: false }),
           tier: new fields.NumberField({ ...requiredTier, initial: 3 })
         }))
       }),
       4: new fields.SchemaField({
-        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-        xpcost: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+        maxPurchasable: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }),
+        xpcost: new fields.NumberField({ ...requiredInteger, initial: 15, min: 0 }),
         allowedKnacks: new fields.ArrayField(new fields.SchemaField({
           uuid: new DocumentUUIDField({ required: true, nullable: false, blank: false }),
           tier: new fields.NumberField({ ...requiredTier, initial: 4 })
